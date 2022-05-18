308099257ab3a6ae48cf20bc0c2fecd6c9485352
moonjs0113 <littlemoom@naver.com>
2022-05-18 20:56:45 +0900

    Rey Repo

---
+++ Rey/yagom-study/Sources/App/Models/Task.swift
@@ -0,0 +1,64 @@
+//
+//  Task.swift
+//  
+//
+//  Created by Moon Jongseek on 2022/05/15.
+//
+
+import Fluent
+import Vapor
+
+final class Task: Model, Content {
+    // DB Table or Collection Name
+    static let schema: String = "tasks"
+    
+    // Model Primary Key
+    @ID(key: .id)
+    var id: UUID?
+    
+    // key: DB Key
+    // 프로퍼티 이름이랑 Key랑 달라도 된다.
+    // Codable 준수
+    @Field(key: "title")
+    var title: String
+    
+    // @Field의 한 종류
+    // DB 열거형으로 저장
+    // Enum이 Optional일 땐, OptionalEnum를 쓴다.
+    @Enum(key: "status")
+    var status: Status
+    
+    // Field가 Optional일 때 쓴다.
+    @OptionalField(key: "comment")
+    var comment: String?
+    
+    // @Field의 한 종류, 설정한 트리거(on:)에 따라 Fluent가 자동으로 설정
+    @Timestamp(key: "created_date", on: .create)
+    var createdDate: Date?
+    
+    // Fluent가 Query에 의해 모델을 생성할 때 사용
+    init() {
+        
+    }
+    
+    init(id: UUID? = nil, title: String, status: Status, comment: String? = nil, createdDate: Date? = nil) {
+        self.id = id
+        self.title = title
+        self.status = status
+        self.comment = comment
+        self.createdDate = createdDate
+    }
+}
+
+enum Status: String, Codable {
+    case toDo, doing, done
+}
+
+
+extension Task: Validatable {
+    static func validations(_ validations: inout Validations) {
+        validations.add("title", as: String.self, is: !.empty)
+        validations.add("status", as: String.self, is: .in("toDo","doing","done"))
+        validations.add("comment", as: String?.self, required: false)
+    }
+}
---
+++ Rey/yagom-study/Sources/App/configure.swift
@@ -0,0 +1,24 @@
+import Fluent
+import FluentPostgresDriver
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    app.databases.use(.postgres(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    ), as: .psql)
+
+    // 만약 Migration끼리 의존성이 있다면,의존성에 따라 나열해야된다.
+    app.migrations.add(CreateTodo())
+    app.migrations.add(TaskMigration())
+
+    // register routes
+    try routes(app)
+}
